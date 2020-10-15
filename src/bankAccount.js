e6abd411bc36a63e6240a5ea3c4fa00cce2869b7
alexis bollia <alexis@bollia.fr>
2020-10-15 10:57:29 +0200

    Add deposit system for bank account client

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,3 +1,9 @@
 export const checkAccountBalance = ({ balance }) => {
     return balance;
 };
+
+export const deposit = (bankAccount, amount) => {
+    const newBalance = bankAccount.balance += amount;
+
+    return { ...bankAccount, balance: newBalance };
+};
--- test/bankAccount.spec.js
+++ test/bankAccount.spec.js
@@ -1,4 +1,4 @@
-import { checkAccountBalance } from '../src/bankAccount';
+import { checkAccountBalance, deposit } from '../src/bankAccount';
 
 describe('bank account system', function() {
     describe('client abilities', () => {
@@ -12,6 +12,27 @@ describe('bank account system', function() {
             const bankAccount = { balance: 100 };
 
             expect(checkAccountBalance(bankAccount)).toStrictEqual(100);
+        });
+    });
+
+    describe('client can deposit', () => {
+        test('account will be credited', () => {
+            let bankAccount = { balance: 0 };
+
+            bankAccount = deposit(bankAccount, 100);
+
+            expect(checkAccountBalance(bankAccount)).toStrictEqual(100);
+        });
+
+        test('account balance increase when credited', () => {
+            let bankAccount = { balance: 0 };
+
+            const previousBalance = checkAccountBalance(bankAccount);
+
+            bankAccount = deposit(bankAccount, 100);
+
+            expect(checkAccountBalance(bankAccount))
+                .toBeGreaterThan(previousBalance);
         });
     });
 });
