67e569e157aed7a79d24699e09fd4f9bd5f8bf84
alexis bollia <alexis@bollia.fr>
2020-10-16 10:43:41 +0200

    Add history operation support

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,44 +1,59 @@
+export const OPERATION = {
+    WITHDRAW: 'Withdraw',
+    DEPOSIT: 'Deposit'
+};
+
+export const getCurrentDate = () => new Date();
+
 export const Account = (() => {
-    const makeAccount = (balance) => {
+    const makeAccount = (balance, history) => {
 
         const _balance = balance.reduce(
             (acc, value) => acc + value
         );
 
+        const _history = history;
+
         const deposit = (amount) => {
-            return makeAccount([...balance, amount]);
+            return makeAccount(
+                [...balance, amount],
+                [...history, {
+                    operation: OPERATION.DEPOSIT,
+                    date: getCurrentDate(),
+                    amount: amount,
+                    balance: _balance + amount
+                }]);
         };
 
         const withdraw = (amount) => {
-            return makeAccount([...balance, -amount]);
+            return makeAccount(
+                [...balance, -amount],
+                [...history, {
+                    operation: OPERATION.WITHDRAW,
+                    date: getCurrentDate(),
+                    amount: amount,
+                    balance: _balance - amount
+                }]
+            );
         };
 
         const getBalance = () => {
             return _balance;
         };
 
+        const getHistory = () => {
+            return _history;
+        };
+
         return Object.freeze({
             deposit,
             withdraw,
-            getBalance
+            getBalance,
+            getHistory
         });
     };
 
-    return (amount) => makeAccount([amount]);
+    return (amount) => makeAccount([amount], []);
 })();
 
-export const checkAccountBalance = ({ balance }) => {
-    return balance;
-};
 
-export const deposit = (bankAccount, amount) => {
-    const newBalance = bankAccount.balance += amount;
-
-    return { ...bankAccount, balance: newBalance };
-};
-
-export const withdraw = (bankAccount, amount) => {
-    const newBalance = bankAccount.balance -= amount;
-
-    return { ...bankAccount, balance: newBalance };
-};
--- test/bankAccount.spec.js
+++ test/bankAccount.spec.js
@@ -1,4 +1,4 @@
-import { Account } from '../src/bankAccount';
+import { Account, OPERATION } from '../src/bankAccount';
 
 describe('bank account system', function() {
     describe('client abilities', () => {
@@ -32,6 +32,56 @@ describe('bank account system', function() {
                 .withdraw(50);
 
             expect(bankAccount.getBalance()).toStrictEqual(50);
+        });
+    });
+
+    describe('client history', () => {
+
+        beforeEach(() => {
+            jest
+                .spyOn(global, 'Date')
+                .mockReturnValue(new Date('2020-10-16T08:38:32.102Z'));
+        });
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+        });
+
+        test('operation appear in bank history', () => {
+
+            let bankAccount = Account(100)
+                .deposit(50);
+
+            expect(bankAccount.getHistory()).toStrictEqual([
+                {
+                    operation: OPERATION.DEPOSIT,
+                    date: new Date(),
+                    amount: 50,
+                    balance: 150
+                }
+            ]);
+        });
+
+        test('multiple operations appear in bank history', () => {
+
+            let bankAccount = Account(100)
+                .deposit(50)
+                .withdraw(20);
+
+            expect(bankAccount.getHistory()).toStrictEqual([
+                {
+                    operation: OPERATION.DEPOSIT,
+                    date: new Date(),
+                    amount: 50,
+                    balance: 150
+                },
+                {
+                    operation: OPERATION.WITHDRAW,
+                    date: new Date(),
+                    amount: 20,
+                    balance: 130
+                }
+            ]);
         });
     });
 });
